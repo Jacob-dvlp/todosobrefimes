c5c76590a61da4b76237e127c2ac27fdff3f3033
jacob-dvlp <robsonpereirawill@gmail.com>
2022-06-09 20:15:29 +0100

    organizando o main

---
+++ lib/app_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp();
+  }
+}
