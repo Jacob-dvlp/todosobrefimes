4e2279b2b2a55eb8f581b99072db88c8441393e4
jacob-dvlp <robsonpereirawill@gmail.com>
2022-06-15 14:24:24 +0100

    adicionando elemento na home

--- lib/pages/home_page/widgets/body_home_footer_list.dart
+++ lib/pages/home_page/widgets/body_home_footer_list.dart
@@ -4,15 +4,25 @@ import '../../../constants/assets_img.dart';
 
 Widget bodyHomeFooterList(context) {
   return Positioned(
-      top: 450,
+      top: 445,
       left: 10,
       right: 10,
       child: SizedBox(
+          height: MediaQuery.of(context).size.height / 3,
           width: MediaQuery.of(context).size.width,
           child: ListView.builder(
+            scrollDirection: Axis.horizontal,
             itemBuilder: (context, index) {
-              return Container(
-                child: Image.asset(splashScreen),
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: MediaQuery.of(context).size.width / 2,
+                  height: 100,
+                  child: Image.asset(
+                    splashScreen,
+                    fit: BoxFit.cover,
+                  ),
+                ),
               );
             },
             shrinkWrap: true,
