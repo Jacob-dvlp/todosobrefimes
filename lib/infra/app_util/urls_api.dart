0908a045a21edd0a523b2e753fd6a0853883cc24
jacob-dvlp <robsonpereirawill@gmail.com>
2022-07-15 04:09:27 +0100

    fixed bug

--- lib/infra/app_util/urls_api.dart
+++ lib/infra/app_util/urls_api.dart
@@ -12,5 +12,5 @@ class ApiUtils {
 class BaseURL {
   static const String img = "https://image.tmdb.org/t/p/original";
   static const String popularUrl =
-      'https://api.themoviedb.org/3/tv/popular?api_key=ddd28b3f6572f3490ff09dafa7a41690&language=en-US&page=1';
+      'https://api.themoviedb.org/3/tv/popular?api_key=ddd28b3f6572f3490ff09dafa7a41690&language=en-US&page=100';
 }
